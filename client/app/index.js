d2e858647d939963c6be0314686bef3974d10dc6
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-12-08 08:20:50 -0300

    cap1 ok

---
+++ client/app/index.js
@@ -0,0 +1,42 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+];
+
+var tbody = document.querySelector('table tbody');
+
+console.log('campos', campos);
+console.log('tbody', tbody);
+
+
+document.querySelector('.form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    var tr = document.createElement('tr');
+
+    campos.forEach(function (campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+
+    var tdVolume = document.createElement('td');
+
+    tdVolume.textContent = campos[1].value * campos[2].value;
+
+    tr.appendChild(tdVolume);
+
+    tbody.appendChild(tr);
+
+
+    // limpa o campo da data
+    campos[0].value = '';
+    // limpa o campo da quantidade
+    campos[1].value = 1;
+    // limpa o campo do valor
+    campos[2].value = 0;
+    // foca no campo da data
+    campos[0].focus();
+});

0b674cc51cc0979bf20067a8e7c161a9c85089ca
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-12-08 10:48:49 -0300

    cap 5 okk

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,22 +5,41 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+
+        this._negociacoes = new Negociacoes();
     }
 
     adiciona(event) {
         // cancelando a submissão do formulário
         event.preventDefault();
 
-        let negociacao = new Negociacao(
-            new Date(DateConverter.paraData(this._inputData.value)),
-            parseInt(this._inputQuantidade.value),
-            parseFloat(this._inputValor.value)
-            );
-            
-            console.log('negociacao', negociacao);
-        
-        let diaMesAno = DateConverter.paraTexto(negociacao.data);
-        console.log(diaMesAno);
+        this.addNegociacao(this._inputData, this._inputQuantidade, this._inputValor);
+        this.limpaFormulario(this._inputData, this._inputQuantidade, this._inputValor);
+
+    }
+
+    addNegociacao(inputData, inputQuantidade, inputValor) {
+        let negociacao = this.criarNegociacao(inputData, inputQuantidade, inputValor);
+        this._negociacoes.adiciona(negociacao);
+        console.log('_negociacoes', this._negociacoes.paraArray());
+        this._negociacoes.paraArray().length = 0;        
+        console.log('_negociacoes', this._negociacoes.paraArray());
+
+    }
+
+    limpaFormulario(inputData, inputQuantidade, inputValor) {
+        inputData.value = '';
+        inputQuantidade.value = 1
+        inputValor.value = 0.0;
+    }
+
+
+    criarNegociacao(inputData, inputQuantidade, inputValor) {
+        return new Negociacao(
+            new Date(DateConverter.paraData(inputData.value)),
+            parseInt(inputQuantidade.value),
+            parseFloat(inputValor.value)
+        );
 
     }
 }

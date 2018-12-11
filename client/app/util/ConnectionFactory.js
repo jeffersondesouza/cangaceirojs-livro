3d9e73e8fed039be5706df55c4d28fb824ca9399
jeffersondesouza <jeffersondesouzacordel@gmail.com>
2018-12-11 19:21:33 -0300

    cap 15

---
+++ client/app/util/ConnectionFactory.js
@@ -0,0 +1,64 @@
+const ConnectionFactory = (() => {
+
+
+    const stores = ['negociacoes'];
+    let connection = null;
+    let close = null;
+
+
+    return class ConnectionFactory {
+
+        constructor() {
+            throw new Error('Static Class, não é possível criar uma instância da mesma')
+        }
+
+
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+
+                if (connection) return resolve(connection);
+
+                const openRequest = indexedDB.open('jscangaceiro', 2);
+
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result);
+                };
+
+                openRequest.onsuccess = e => {
+                    connection = e.target.result;
+
+                    close = connection.close.bind(connection);
+
+                    connection.close = () => {
+                        throw new Error('Você não pode fechar diretamente a conexão');
+                    };
+
+                    resolve(connection);
+                };
+
+                openRequest.onerror = e => reject(e.target.error.name);
+
+            })
+        }
+
+
+        static closeConnection() {
+            if (connection) {
+                close();
+            }
+        }
+
+
+        static _createStores(connection) {
+            stores.forEach(store => {
+                // if sem bloco, mais sucinto!
+                if (connection.objectStoreNames.contains(store))
+                    connection.deleteObjectStore(store);
+                connection.createObjectStore(store, {
+                    autoIncrement:
+                        true
+                });
+            });
+        }
+    }
+})();
